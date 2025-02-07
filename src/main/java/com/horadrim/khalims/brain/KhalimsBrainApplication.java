876ccb5ce4fd5464a3cc2f509072b79e705c2145
TalRasha <524948250@qq.com>
2025-02-07 14:15:10 +0800

    init spring boot 3 project

---
+++ src/main/java/com/horadrim/khalims/brain/KhalimsBrainApplication.java
@@ -0,0 +1,13 @@
+package com.horadrim.khalims.brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhalimsBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhalimsBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horadrim/khalims/brain/KhalimsBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horadrim.khalims.brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhalimsBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
