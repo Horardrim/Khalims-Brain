dc300bc1d33f6b1e6da784c409f9a5ca7aac357b
TalRasha <524948250@qq.com>
2025-08-19 03:33:10 +0000

    fix unit test error of introducing flyway

--- src/test/java/com/horadrim/khalims/brain/bean/ComponentAnnoBeanTest.java
+++ src/test/java/com/horadrim/khalims/brain/bean/ComponentAnnoBeanTest.java
@@ -2,11 +2,14 @@ package com.horadrim.khalims.brain.bean;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ComponentAnnoBean.class)
 @Slf4j
 public class ComponentAnnoBeanTest {
 
