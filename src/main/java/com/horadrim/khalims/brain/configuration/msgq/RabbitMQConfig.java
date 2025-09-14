d241861bbe615c9a28ad9145b453dc6040bbffe0
Tal Rasha <524948250@qq.com>
2025-09-14 12:21:26 +0000

    delete unneeded queue and bind

--- src/main/java/com/horadrim/khalims/brain/configuration/msgq/RabbitMQConfig.java
+++ src/main/java/com/horadrim/khalims/brain/configuration/msgq/RabbitMQConfig.java
@@ -20,40 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 public class RabbitMQConfig {
     /** direct exchange **/
     @Bean
-    public Queue queue() {
-        return new Queue(RabbitMQConstants.QUEUE_NAME, true);
-    }
-
-    @Bean
     public DirectExchange exchange() {
         return new DirectExchange(RabbitMQConstants.EXCHANGE, true, false);
-    }
-
-    @Bean
-    public Binding binding() {
-        return BindingBuilder
-                .bind(queue())
-                .to(exchange())
-                .with(RabbitMQConstants.ROUTING_KEY);
     }
 
     /** topic exchange **/
     @Bean
     public TopicExchange topicExchange() {
         return new TopicExchange(RabbitMQConstants.TOPIC_EXCHANGE, true, false);
-    }
-
-    @Bean
-    public Queue topicQueue() {
-        return new Queue(RabbitMQConstants.QUEUE_NAME_TOPIC_EXCHANGE, true);
-    }
-
-    @Bean
-    public Binding topicExchangeBind() {
-        return BindingBuilder
-                .bind(topicQueue())
-                .to(topicExchange())
-                .with(RabbitMQConstants.TOPIC_EXCHANGE_ROUTING_KEY);
     }
 
     @Bean
